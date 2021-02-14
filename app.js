6b82b4cf872f47b1b8de482ab0a34fcdd78f8cf5
kazi maruf <kazimaruf008@example.com>
2021-02-14 09:10:41 +0400

    r

--- app.js
+++ app.js
@@ -48,15 +48,12 @@ const getImages = (query) => {
 let slideIndex = 0;
 const selectItem = (event, img) => {
   let element = event.target;
-  element.classList.add('added');
+  element.classList.toggle('added');
  
   let item = sliders.indexOf(img);
   if (item === -1) {
     sliders.push(img);
-  } else {
-    alert('Hey, Already added !')
-    sliders.pop(img);
-  }
+   }
 }
 var timer
 const createSlider = () => {
